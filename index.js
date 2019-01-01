847796d5b3b8cf15ca58389015676a44478a9f48
devchain 007 <p@wscode.info>
2019-01-01 18:30:26 +0545

    added es6 feature

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var ones = [ 
+  "", "one ", "two ", "three ", "four ",  "five ", "six ", "seven ", "eight ",  
+  "nine ", "ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", 
+  "sixteen ", "seventeen ", "eighteen ", "nineteen "
+]
+
+var tens = [ "", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety " ]
+
+function convertNumberToWords(num){
+  var convertedWords = "";
+  convertedWords += numToWords((num/10000000), "crore ");
+  convertedWords += numToWords(((num/100000) % 100), "lakh ");
+  convertedWords += numToWords(((num/1000) % 100), "thousand ");
+  convertedWords += numToWords(((num/100) % 10), "hundred ");
+
+  if(num > 100 && num % 100){
+    convertedWords += "and ";
+  }
+
+  convertedWords += numToWords((num % 100), "");
+
+  return convertedWords;
+
+}
+
+function numToWords(num, places){
+  var currentPlacesText = "";
+  
+  num = parseInt(num);
+
+  if(num > 19){
+    currentPlacesText = tens[parseInt(num/10)] + ones[num % 10];
+  }else{
+    currentPlacesText  = ones[parseInt(num)];
+  }
+
+  if(num){
+    currentPlacesText +=places;
+  }
+
+  return currentPlacesText;
+}
+
+
+console.log(convertNumberToWords(438237764));
